54db6bd03eff3792b417f9436bf7e1354dd8720e
Mitsu Hadeishi <mitsu@syntheticzero.com>
2015-10-31 14:07:21 -0400

    Re #4 Initial, partially-completed deployment scripts for Oura cloud server.

---
+++ deploymentscripts/installwebapp/java.sh
@@ -0,0 +1,9 @@
+export JAVA_HOME=/opt/jdk
+export JETTY_HOME=/opt/jetty
+export ANT_OPTS="-Xms128m -Xmx512m"
+export GRAILS_OPTS="-server -Xmx3000M -XX:MaxPermSize=256m -Dfile.encoding=UTF-8 -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC"
+export VISUAL=vi
+export PATH USER LOGNAME MAIL HOSTNAME HISTSIZE INPUTRC
+export CLASSPATH=.:$JAVA_HOME/lib/classes.zip
+
+PATH=$PATH:/home/oura/grails/bin:/home/oura/bin:/usr/share/java/bin
---
+++ deploymentscripts/installwebapp/localconfig/LocalConfig_template.groovy
@@ -0,0 +1,8 @@
+// Need to update this local config to use mongo configuration
+
+dataSource {
+	url = "jdbc:mysql://curiousdb/DBNAME?zeroDateTimeBehavior=convertToNull"
+	username = "DBUSERNAME"
+	password = "DBPASSWORD"
+}
+grails.serverURL = "https://APPHOSTNAME/"
