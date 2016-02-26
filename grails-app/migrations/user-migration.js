4da38fdf4b3d865d779ca86f2cdb813c994b7553
Shashank Agrawal <shashank.agrawal@causecode.com>
2016-02-26 15:50:33 +0530

    Added migration to give admin & client role to various users.

--- grails-app/migrations/user-migration.js
+++ grails-app/migrations/user-migration.js
@@ -5,3 +5,30 @@
 db.user.find().snapshot().forEach(function (userInstance) {
     db.user.update({_id: userInstance._id}, {$set: {username: userInstance.email}});
 });
+
+// changeset Shashank:adding-admin-client-role-02262016
+
+var emails = ["sfkate@mac.com", "mitsu@syntheticzero.com", "mitsu@wearecurio.us", "mitsu@ministryofthought.com",
+        "teemu.kurppa@gmail.com", "oura@causecode.com"];
+
+var roleClientManager = db.role.findOne({authority: "ROLE_CLIENT_MANAGER"});
+var roleAdmin = db.role.findOne({authority: "ROLE_ADMIN"});
+
+var dbRef = db;
+function counterId() {
+    // Every domain has a subsequent domain suffixed with "next_id" to maintain next identifier.
+    var ret = dbRef.userRole.next_id.findAndModify({query: {_id: "userRole"}, update: {$inc: {next_id: 1}}, new: true, upsert: true});
+    return ret.next_id;
+}
+
+db.user.find({email: {$in: emails}}).snapshot().forEach(function(userInstance) {
+    db.userRole.insert([{
+        _id: NumberLong(counterId()),
+        role: roleClientManager._id,
+        user: userInstance._id
+    }, {
+        _id: NumberLong(counterId()),
+        role: roleAdmin._id,
+        user: userInstance._id
+    }]);
+});
